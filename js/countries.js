89879410dbdcf1b5e85ea704b7bf657df45daba4
nizihad824 <mdnurulislam97@gmail.com>
2022-03-21 01:12:41 +0100

    rest countrie api

---
+++ js/countries.js
@@ -0,0 +1,26 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+const displayCountries = countries => {
+    // console.log(countries[0])
+    const allCountriesHTML = countries.map(country => getCountryHTML(country))
+    // console.log(allCountriesHTML[0])
+    const container =document.getElementById('countries');
+    container.innerHTML = allCountriesHTML.join(' ')
+
+
+}
+const getCountryHTML = country => {
+    // console.log(country)
+    return `
+            <div class ="country">
+                <h2>${country.name.common}</h2>
+                <img src="${country.flags.png}">
+            </div>
+        
+    `
+
+}
+loadCountries()
